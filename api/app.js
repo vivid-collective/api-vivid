f63f9c42884c701ae278c637acc58d153ccb1259
matt-winzer <matt.winzer@gmail.com>
2018-01-25 18:32:41 -0700

    added monk, removed views folder

--- api/app.js
+++ api/app.js
@@ -5,14 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const monk = require('monk');
+const url = 'localhost/vivid';
+const db = monk(url);
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+db.then(() => {
+  console.log('Connected correctly to server')
+})
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
