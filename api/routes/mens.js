0d808e83d89e4103846281a9ac8901a9e23c321f
matt-winzer <matt.winzer@gmail.com>
2018-01-25 22:36:59 -0700

    mens routes get all, get by model name, get by id working

--- api/routes/mens.js
+++ api/routes/mens.js
@@ -1,30 +1,33 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 // Database connection
-const monk = require('monk');
-const url = 'localhost/vivid';
-const db = monk(url);
+const monk = require('monk')
+const url = 'localhost/vivid'
+const db = monk(url)
 
 // Collections
 const mens = db.get('mens')
 
+// Routes
 
-
-// Seed database collections
-router.get('/', (req, res, next)=>{
-  mens.find()
-    .then((mens)=>{
-      res.json(mens)
-    })
+// GET All
+router.get('/', (req, res, next) => {
+  mens.find().then(mens => res.json(mens))
 })
 
-//By id 
+// GET One by model querystring
+router.get('/search', (req, res, next) => {
+  if (req.query.model) {
+    mens.findOne({ 'sunglass.model': req.query.model }).then(model => res.json(model))
+  } else {
+    res.json({ error: 'Please enter search term' })
+  }
+})
+
+// GET One by id
 router.get('/:id', (req, res, next) => {
-  mens.findOne({ _id : req.params.id })
-    .then((model) => {
-      res.json(model)
-    })
+  mens.findOne({ _id: req.params.id }).then(model => res.json(model))
 })
 
-module.exports = router;
+module.exports = router
