5dcf8147f80109099c13e869785c19e6c4fe251b
matt-winzer <matt.winzer@gmail.com>
2018-01-25 22:29:17 -0700

    code craft & no semicolons womens routes

--- api/routes/womens.js
+++ api/routes/womens.js
@@ -1,43 +1,33 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 // Database connection
-const monk = require('monk');
-const url = 'localhost/vivid';
-const db = monk(url);
+const monk = require('monk')
+const url = 'localhost/vivid'
+const db = monk(url)
 
 // Collections
 const womens = db.get('womens')
 
+// Routes
 
-
-// Seed database collections
+// GET All
 router.get('/', (req, res, next) => {
-    womens.find()
-        .then((womens) => {
-            res.json(womens)
-        })
+    womens.find().then(womens => res.json(womens))
 })
 
+// GET One by model querystring
 router.get('/search', (req, res, next) => {
     if (req.query.model) {
-        womens.findOne({ 'sunglass.model': req.query.model })
-            .then(model => {
-                res.json(model)
-            })
+        womens.findOne({ 'sunglass.model': req.query.model }).then(model => res.json(model))
     } else {
-        res.json({ error: 'Please enter search term'})
+        res.json({ error: 'Please enter search term' })
     }
-});
-
-// get by id
-router.get('/:id', (req, res, next) => {
-    womens.findOne({
-            _id: req.params.id
-        })
-        .then((model) => {
-            res.json(model)
-        })
 })
 
-module.exports = router;
+// GET One by id
+router.get('/:id', (req, res, next) => {
+    womens.findOne({ _id: req.params.id }).then(model => res.json(model))
+})
+
+module.exports = router
