887c7cab8f74f00a8f32b4065d0d55bf4046eff2
matt-winzer <matt.winzer@gmail.com>
2018-01-25 22:38:49 -0700

    goggles routes get all, get by model name, get by id working

--- api/routes/goggles.js
+++ api/routes/goggles.js
@@ -9,21 +9,25 @@ const db = monk(url);
 // Collections
 const goggles = db.get('goggles')
 
+// Routes
 
-
-// Seed database collections
+// GET All
 router.get('/', (req, res, next) => {
-    goggles.find()
-        .then((goggles) => {
-            res.json(goggles)
-        })
+    goggles.find().then(goggles => res.json(goggles))
 })
 
+// GET One by model querystring
+router.get('/search', (req, res, next) => {
+    if (req.query.model) {
+        goggles.findOne({ 'sunglass.model': req.query.model }).then(model => res.json(model))
+    } else {
+        res.json({ error: 'Please enter search term' })
+    }
+})
+
+// GET One by id
 router.get('/:id', (req, res, next) => {
-    goggles.findOne({_id : req.params.id })
-        .then((goggle) => {
-            res.json(goggle)
-        })
+    goggles.findOne({ _id: req.params.id }).then(model => res.json(model))
 })
 
 module.exports = router;
